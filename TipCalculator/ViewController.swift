da6a5a3b386805e0846bed9a5e141ecb77d6d151
Dwayyne W Anthony <dwaynewanthony@gmail.com>
2019-12-31 13:37:07 -0500

    Added calculations

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,24 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var totalwithTipFIeld: UILabel!
     @IBOutlet weak var tipField: UILabel!
     @IBOutlet weak var button20: UIButton!
     @IBOutlet weak var button15: UIButton!
     @IBOutlet weak var button10: UIButton!
     @IBOutlet weak var billTextField: UITextField!
+    
+    var percentageOfTip = Float(0.0)
+    var amountDue = Float(0.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-    @IBAction func calculatetip(_ sender: Any) {
-        if billTextField != nil{
-            if button10.isSelected{
-                var total = Float(billTextField.text) * 10
-            }
+    
+    @IBAction func calculatetip(_ sender: UIButton) {
+        let totalBill = Float(billTextField.text!) ?? 0
+        
+        if(button10.isSelected){
+            percentageOfTip = Float(0.1)
         }
+        else if(button15.isSelected){
+            percentageOfTip = Float(0.15)
+        }
+        else{
+            percentageOfTip = Float(0.20)
+            print(percentageOfTip)
+            
+        }
+        amountDue = totalBill * (1 + percentageOfTip)
+        //print("Total Due: \(amountDue)")
+        print(String(format: "Total Due: %.2f", amountDue))
     }
     
 }
