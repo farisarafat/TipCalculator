36c2036d4b1d900ea394c6e5aa4317c0fd9e0b7e
farisarafat <farisa97@gmail.com>
2019-12-31 13:06:00 -0500

    stuff in code side

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalwithTipFIeld: UILabel!
+    @IBOutlet weak var tipField: UILabel!
+    @IBOutlet weak var button20: UIButton!
+    @IBOutlet weak var button15: UIButton!
+    @IBOutlet weak var button10: UIButton!
+    @IBOutlet weak var billTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculatetip(_ sender: Any) {
+        if billTextField != nil{
+            if button10.isSelected{
+                var total = Float(billTextField.text) * 10
+            }
+        }
+    }
+    
 }
 
